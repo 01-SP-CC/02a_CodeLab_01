294af111ebe83d7da84530b3fd8daf0ce8ab9329
sven-pke <sven-perschke@gmx.de>
2023-05-23 15:58:13 +0200

    läuft schonmal

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,17 +1,34 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
-    
-       
+        // Erzeugung von 3 Konto-Instanzen
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
+
+        // Ausgabe der Anfangskontostände
+        output("Anfangskontostand konto1: " + konto1.getKontostand());
+        output("Anfangskontostand konto2: " + konto2.getKontostand());
+        output("Anfangskontostand konto3: " + konto3.getKontostand());
+
+        // Änderung der Kontostände
+        konto1.verdoppeln();
+        konto2.verdreifachen();
+        konto3.verzehnfachen();
+
+        // Ausgabe der aktuellen Kontostände
+        output("Aktueller Kontostand konto1: " + konto1.getKontostand());
+        output("Aktueller Kontostand konto2: " + konto2.getKontostand());
+        output("Aktueller Kontostand konto3: " + konto3.getKontostand());
     }
 
 
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
-    } 
-
+        public static void output(String outputStr) {
+            System.out.println(outputStr);
+        }
+    
 
 }
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,35 @@
+package com.cc.java;
+
+public class Konto {
+    // Instanzvariable kontostand
+    private int kontostand;
+
+    // Konstruktor
+    public Konto() {
+        // Anfangswert 10000 setzen
+        this.kontostand = 10000;
+    }
+
+    // Getter-Methode für kontostand
+    public int getKontostand() {
+        return this.kontostand;
+    }
+
+    // Methode zur Verdoppelung des Kontostands
+    public void verdoppeln() {
+        this.kontostand *= 2;
+    }
+
+    // Methode zur Verdreifachung des Kontostands
+    public void verdreifachen() {
+        this.kontostand *= 3;
+    }
+
+    // Methode zur Verzehnfachung des Kontostands
+    public void verzehnfachen() {
+        this.kontostand *= 10;
+    }
+}
+
+
+
